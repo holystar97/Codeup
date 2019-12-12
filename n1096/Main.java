ade855b92d0453071db71848a47749e0aab1d659
holystar97 <35961898+holystar97@users.noreply.github.com>
2019-12-12 21:42:09 +0900

    Update Main.java

--- n1096/Main.java
+++ n1096/Main.java
@@ -22,11 +22,11 @@ public class Main {
 		}
 		
 		for(int i=1; i<=19; i++) {
-			
+			for(int j=1; j<=19; j++) {
+				System.out.printf("%d ",input[i][j]);
+			}
+			System.out.println(" ");
 		}
-		
-		
-		
 		
 	}
 
